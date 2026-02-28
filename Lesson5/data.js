bb005e9fc848b9c183284b2be666170190edf28e
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-02-28 14:24:09 +0700

    Lesson 5

---
+++ Lesson5/data.js
@@ -0,0 +1,8 @@
+const students = [
+    {id: 1, name: 'Tiến Dũng', age: 13},
+    {id: 2, name: 'Minh Ngọc', age: 13},
+    {id: 3, name: 'Đức Minh', age: 13},
+    {id: 4, name: 'Ngọc Huy', age: 14},
+    {id: 5, name: 'Quang Minh', age: 14},
+    {id: 6, name: 'Minh Quân', age: 14}
+];
