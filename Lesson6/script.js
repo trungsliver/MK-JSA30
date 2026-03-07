99c1ec3579df52b884b2ed60e41d1c80e4ae59fa
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-03-07 15:48:51 +0700

    Lesson 6

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -25,7 +25,7 @@ fetch('https://jsonplaceholder.typicode.com/todos')
         }
     });
 
-// Post list
+// Bài 2: Post list
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(data => {
@@ -41,3 +41,26 @@ fetch('https://jsonplaceholder.typicode.com/posts')
             container.appendChild(card);
         }
     });
+
+// Bài 3: Dark mode - Light mode
+const themeBtn = document.getElementById('themeBtn');
+
+    // Khi ấn nút
+themeBtn.onclick = () => {
+    // Thêm hoặc xóa class 'dark' cho body
+    document.body.classList.toggle('dark');
+    // Lưu trạng thái hiện tại vào localStorage
+    if (document.body.classList.contains('dark')) {
+        // Lưu trạng thái dark vào localStorage
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+    // load theme từ loacalStorage khi reload trang
+        // Lấy theme đã lưu từ localStorage
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark');
+}
